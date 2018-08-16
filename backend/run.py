6a54642d696978fc94be94368a68b45a66e5fa4d
shepherd_dc <shepherd_dc@aliyun.com>
2018-08-16 11:00:27 +0800

    'threaded=True'

--- backend/run.py
+++ backend/run.py
@@ -45,4 +45,4 @@ def params_test(params):
     return jsonify({'params': params})
  
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host='0.0.0.0', port=5050, threaded=True)
